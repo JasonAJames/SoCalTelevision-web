1fbfd5d1a7000f627e0db1384ae65386233630bd
Jason James <jasonajames@live.com>
2018-03-22 20:14:30 -0700

    working on Routing app.component.html adding imports

--- app/ts/app.routes.ts
+++ app/ts/app.routes.ts
@@ -1,14 +1,14 @@
 import { provideRouter, RouterConfig } from '@angular/router';
-import { HomeComponent } from './ts/home.component';
-import { AppComponent } from './ts/app.component';
-import { AppComponent1 } from './ts/app.component.1';
+import { HomeComponent } from './home.component';
+import { AppComponent } from './app.component';
+import { AppComponent1 } from './app.component.1';
 
 const routes: RouterConfig = [
     { path:'', redirectTo: 'home'},
     { path: 'home', component: 'HomeComponent'},
     { path:'about', component: 'AppComponent'},
     { path:'ktla5', component: 'AppComponent1'},
-    { path:'**', component: 'AppComponent'}
+    { path:'**', component: 'HomeComponent'}
     
 
 ];
--- app/ts/main.ts
+++ app/ts/main.ts
@@ -1,5 +1,7 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {myRouterProviders} from '../app.routes';
+import {myRouterProviders} from './app.routes';
+import {HomeComponent} from './home.component';
 import {AppComponent} from './app.component';
+import {AppComponent1} from './app.component.1';
 
 bootstrap(AppComponent, [myRouterProviders]);
